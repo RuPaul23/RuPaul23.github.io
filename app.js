2b7c89e72a4449bfe1356c9e619204c6529bfda0
RuPaul <105512611+RuPaul23@users.noreply.github.com>
2023-02-01 23:47:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,51 @@
-//required packages
 const express = require("express");
 const fetch = require("node-fetch");
-//const ejsLint = require('ejs-lint');
 require("dotenv").config();
 
-//create the express server
-const app =express();
-
-//server port number
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-//set template engine
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
-//needed to parse html data for POST request
 app.use(express.urlencoded({
     extended:true
 }))
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.render("index")
-})
-app.post("/convert-mp3", async (req, res) => 
-{
-    const videoId = req.body.videoID;
-    if
-    (
-        videoId === undefined ||
-        videoId === "" ||
-        videoId === null
-    )
-    {
-        return res.render("index", {sucess : false, message : "Please enter a valid video ID"});
-    }
-    else
-    {
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/convert-mp3", async (req, res) => {
+    let videoId = req.body.videoID;
+    if (videoId === undefined || videoId === "" || videoId === null) {
+        return res.render("index", { sucess: false, message: "Please enter a valid video ID" });
+    } else {
+        // Extract the video ID from the full YouTube link
+        if (videoId.includes("watch?v=")) {
+            videoId = videoId.split("watch?v=")[1];
+        } else if (videoId.includes("youtu.be/")) {
+            videoId = videoId.split("youtu.be/")[1];
+        }
+
         const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`,
-        {
-            "method" : "GET",
-            "headers" : 
             {
-                "x-rapidapi-key" : process.env.API_KEY,
-                "x-rapidapi-host" : process.env.API_HOST
-            }
-        });
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": process.env.API_KEY,
+                    "x-rapidapi-host": process.env.API_HOST
+                }
+            });
         const fetchResponse = await fetchAPI.json();
 
-        if(fetchResponse.status === "ok")
-            return res.render("index", { sucess: true, song_title: fetchResponse.title, song_link : fetchResponse.link});
-        else
-            return res.render("index",{sucess: false, message: fetchResponse.msg})
+        if (fetchResponse.status === "ok") {
+            return res.render("index", { sucess: true, song_title: fetchResponse.title, song_link: fetchResponse.link });
+        } else {
+            return res.render("index", { sucess: false, message: fetchResponse.msg });
+        }
     }
-})
-    
+});
 
-//start the server
-app.listen(PORT,()=>
-{
+app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
-})
+});
